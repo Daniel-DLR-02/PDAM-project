da8dd82a0a857ab33cbe11998e08575f16ce1bdf
Daniel <danidlr02@gmail.com>
2022-03-17 19:22:30 +0100

    First commit

---
+++ tcl/src/main/java/com/pdam/tcl/TclApplication.java
@@ -0,0 +1,13 @@
+package com.pdam.tcl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TclApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TclApplication.class, args);
+	}
+
+}
---
+++ tcl/src/test/java/com/pdam/tcl/TclApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdam.tcl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TclApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
