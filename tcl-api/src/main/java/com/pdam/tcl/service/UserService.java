4f10a5ba739362e1837d25a1e8f5877f41fea213
Daniel-DLR-02 <danidlr02@gmail.com>
2022-05-22 19:30:47 +0200

    Changed path of repos

--- tcl-api/src/main/java/com/pdam/tcl/service/UserService.java
+++ tcl-api/src/main/java/com/pdam/tcl/service/UserService.java
@@ -2,9 +2,6 @@ package com.pdam.tcl.service;
 
 import com.pdam.tcl.model.User;
 import com.pdam.tcl.model.dto.user.CreateUserDto;
-import com.pdam.tcl.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
