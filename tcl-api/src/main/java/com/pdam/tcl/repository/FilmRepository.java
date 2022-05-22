4a4427057284103d38aaf86b7e3b3ba647a38442
Daniel-DLR-02 <danidlr02@gmail.com>
2022-05-22 20:50:51 +0200

    Dto query fix

--- tcl-api/src/main/java/com/pdam/tcl/repository/FilmRepository.java
+++ tcl-api/src/main/java/com/pdam/tcl/repository/FilmRepository.java
@@ -17,7 +17,7 @@ public interface FilmRepository extends JpaRepository<Film, UUID> {
                             f.uuid,f.title,f.description,f.duration,f.releaseDate,f.genre
                         ) from Film f
                         where CURRENT_DATE between f.releaseDate and f.expirationDate
-                    """,nativeQuery = true)
+                    """)
     Page<GetFilmDto> findCurrentFilms(Pageable pageable);
 
 }
