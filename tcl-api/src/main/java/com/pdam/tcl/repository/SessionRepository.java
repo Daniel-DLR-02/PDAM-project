b172515106ed878b3aa48094b6d6dce610288005
Daniel de Luna Rodríguez <danidlr02@gmail.com>
2022-06-10 08:36:14 +0200

    session order

--- tcl-api/src/main/java/com/pdam/tcl/repository/SessionRepository.java
+++ tcl-api/src/main/java/com/pdam/tcl/repository/SessionRepository.java
@@ -20,6 +20,7 @@ public interface SessionRepository extends JpaRepository<Session, UUID> {
                         FROM Session s JOIN Film f ON s.film.uuid = f.uuid
                         JOIN Hall h ON s.hall.uuid = h.uuid
                         WHERE s.film.uuid = :filmUuid
+                        ORDER BY s.sessionDate ASC
                     """)
     Page<GetSessionDto> getSessionsByFilmId(UUID filmUuid, Pageable pageable);
 
@@ -28,6 +29,7 @@ public interface SessionRepository extends JpaRepository<Session, UUID> {
                         FROM Session s JOIN Film f ON s.film.uuid = f.uuid
                         JOIN Hall h ON s.hall.uuid = h.uuid
                         WHERE s.film.uuid = :filmUuid
+                        ORDER BY s.sessionDate ASC
                     """)
     List<Session> getSessionsByFilmIdList(UUID filmUuid);
 
@@ -38,6 +40,7 @@ public interface SessionRepository extends JpaRepository<Session, UUID> {
                         ) 
                         FROM Session s JOIN Film f ON s.film.uuid = f.uuid
                         JOIN Hall h ON s.hall.uuid = h.uuid
+                        ORDER BY s.sessionDate ASC
                     """)
     Page<GetSessionDto> findAllSessions(Pageable pageable);
 
